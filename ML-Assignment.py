720289a5d196d64e4818d6fc26a6f1bad50bfe64
NamrathaRavula <nxr03000@ucmo.edu>
2022-10-18 18:52:54 -0500

    ML class Assignment

---
+++ ML-Assignment.py
@@ -0,0 +1,42 @@
+#Question 1
+from numpy import random;
+import numpy as np;
+a=random.randint(20, size=(15))
+b = a.reshape(3,5)
+print(b)
+new_a = np.where(b == [
+   [i]
+   for i in np.amax(b, axis = 1)
+], 0 ,b)
+print(new_a)
+
+
+
+#Question 2
+import pandas as pd
+data = pd.read_csv("C:/Users/sruja/OneDrive/Desktop/python-codes/data.csv")
+data.head()
+data.describe()
+data = data.fillna(data.mean())
+print(data[['Maxpulse','Calories']].agg(['min','max','mean','count']))
+print(data[(data.Calories < 1000) & (data.Calories > 500)]) 
+print(data[ (data.Calories>500) & (data.Pulse < 100)])
+df_modified = pd.DataFrame(data, columns = ['Duration', 'pulse', 'Calories'])
+print (df_modified.head())
+del data["Maxpulse"]
+print(data.head)
+data['Calories'] = data['Calories'].astype("int")
+print(data['Calories'].dtypes)
+data.plot.scatter(x ='Duration', y= 'Calories')
+
+
+#Question 3
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([22.2, 17.6, 8.8, 8, 7.7, 6.7])
+mylabels = ["Java", "Python", "PHP", "JavaScript", "C#", "C++"]
+myexplode = [0.2, 0, 0, 0,0,0]
+plt.pie(y,labels=mylabels, explode = myexplode,autopct='%1.1f%%')
+plt.show()
+
